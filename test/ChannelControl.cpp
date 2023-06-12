57419c32d0825e8b6ea6a7ed6a9b23866ae4420b
Gyeongju Kim <gyeokim@c2r10s4.42seoul.kr>
2023-06-12 11:16:52 +0900

    6/12 done

--- test/ChannelControl.cpp
+++ test/ChannelControl.cpp
@@ -5,14 +5,14 @@
 
 ChannelControl::ChannelControl()
 {
-    this->_channel_map = new std::map<std::string, Channel *>;
+    this->_channel_map = new std::map<const std::string, Channel *>;
 
     std::cout << std::setw(15) << "[ChannelControl] " << "create!!" << std::endl;
 }
 
 ChannelControl::ChannelControl(const ChannelControl& ref)
+    :_channel_map(ref._channel_map)
 {
-    *this=ref;
     std::cout << std::setw(15) << "[ChannelControl] " << "copy!!" << std::endl;
 }
 
@@ -23,7 +23,25 @@ ChannelControl::~ChannelControl()
 
 ChannelControl&	ChannelControl::operator=(const ChannelControl& ref)
 {
-    *this=ref;
+    this->_channel_map = ref._channel_map;
     std::cout << std::setw(15) << "[ChannelControl] " << "operator=!!" << std::endl;
     return (*this);
+}
+
+
+
+
+void ft_append_channel(const Channel &ch)
+{
+
+}
+
+
+
+Channel *ChannelControl::ft_get_channel(const std::string &channel_name)
+{
+	std::map<const std::string, Channel *>::iterator it = this->_channel_map->find(channel_name);
+	if (it == this->_channel_map->end())
+		return (NULL);
+	return (this->_channel_map->at(channel_name));
 }
--- test/ChannelControl.hpp
+++ test/ChannelControl.hpp
@@ -2,14 +2,16 @@
 # define CHANNELCONTROL_HPP
 
 #include <map>
-#include <Channel.hpp>
+#include <string>
+#include "Channel.hpp"
+
 
 class ChannelControl
 {
 private:
     /* data */
 
-	std::map<const std::string, Channel *> _channel_map;
+	std::map<const std::string, Channel *> *_channel_map;
 
     
 public:
@@ -18,6 +20,15 @@ public:
     ~ChannelControl();
 
     ChannelControl&	operator=(const ChannelControl& ref);
+
+    void ft_append_channel(const Channel &ch);
+    Channel * ft_get_channel(const std::string &channel_name);
+    ChannelControl &ft_get_channel_control_class() { return (*this); }
+
+
+
+
+    std::map<const std::string, Channel *> *ft_get_channel_map() { return this->_channel_map; }
 };
 
 #endif
